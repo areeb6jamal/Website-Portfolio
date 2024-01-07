19c967236ba44fd7ae567f3524c8fc4812938aa0
areeb6jamal <90337934+areeb6jamal@users.noreply.github.com>
2024-01-06 21:29:57 -0500

    Setup Skills Page Layout

--- src/about/subheadingsData.js
+++ src/about/subheadingsData.js
@@ -5,7 +5,15 @@ const subheadingsData = {
       content: (
         <>
           <p>
-            Hi! I'm <b>Areeb Jamal</b>.
+            Hi! I'm <b><k>Areeb Jamal</k></b>!
+          </p>
+          <p>
+            Welcome to my small <b><k>space</k></b> of the internet.
+          </p>
+          <p>
+          When I am not busy exploring the universe, I like to settle back down on Earth,
+          where I live day to day looking for new technologies to explore. 
+          I love expanding my knowledge about computer hardware and software!
           </p>
         </>
       ),
@@ -13,7 +21,7 @@ const subheadingsData = {
   ],
   2: [
     {
-      title: "Bachelor of Computer Engineering & Management",
+      title: "B.Eng. Computer + Management",
       content: (
         <>
           <p>
@@ -22,8 +30,8 @@ const subheadingsData = {
           <p>
           Currently, I am in my fourth-year with a 3.8 GPA and recognized by the world's largest honour society, 
           the Golden Key International Honour Society for being in the top 15% of my program. 
-          The Engineering & Management Program at McMaster is a prestigious 5-year program designed to provide 
-          engineering students with a deeper understanding  of the business, project management, 
+          The Computer Engineering & Management Program at McMaster is a prestigious 5-year program designed to provide 
+          engineering students with a deeper understanding of the business, project management, 
           and leadership skills needed to thrive in a corporate or entrepreneurial environment. 
           </p>
         </>
@@ -32,29 +40,37 @@ const subheadingsData = {
   ],
   3: [
     {
-      title: "Wireless Network Intern",
+      title: "Wireless Network Operations Intern",
       content: (
         <>
           <p>
             <b>Bell Canada: May 2023 - Aug. 2023</b>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          - Administered and launched 9 capital projects across the GTHA alongside 800+ preexisting cell sites contributing to
+          efficiently delivering quality fiber and cellular network coverage to 99% of Canadians.
+          </p>
+          <p>
+          - Streamlined troubleshooting processes through creative innovations alongside advising a team of 34 technicians on
+          prioritization strategies which efficiently saved company time by 11%.
           </p>
         </>
       ),
     },
     {
-      title: "Radio Frequency Network Engineer Intern",
+      title: "RF Network Engineer Intern",
       content: (
         <>
           <p>
             <b>Ericsson Canada: May 2022 - Aug. 2022</b>
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          - Monitored a variety of radio frequency signals and bands for 300+ sites across the city of Jacksonville remotely and
+          generated detailed site summaries for customers.
+          </p>
+          <p>
+          - Analyzed high VSWR through KPIs to further optimize radio network performance which boosted the efficiency of available
+          network resources by 24%.
           </p>
         </>
       ),
---
+++ src/skills/skillsData.js
@@ -0,0 +1,69 @@
+const skills = {
+    1: [
+      {
+        title: "C/C++",
+        level: 6,
+      },
+      {
+        title: "Python",
+        level: 6,
+      },
+      {
+        title: "Java",
+        level: 5,
+      },
+      {
+        title: "SQL",
+        level: 2,
+      },
+      {
+        title: "HTML",
+        level: 6,
+      },
+      {
+        title: "CSS",
+        level: 6,
+      },
+      {
+        title: "Javascript",
+        level: 3,
+      },
+      {
+        title: "React JS",
+        level: 2,
+      },
+    ],
+    2: [
+      {
+        title: "Verilog",
+        level: 5,
+      },
+      {
+        title: "SystemVerilog",
+        level: 4,
+      },
+      {
+        title: "FPGAs",
+        level: 2,
+      },
+      {
+        title: "Oscilloscopes",
+        level: 4,
+      },
+      {
+        title: "Multimeters",
+        level: 2,
+      },
+      {
+        title: "Amplifiers",
+        level: 3,
+      },
+      {
+        title: "Rectifiers",
+        level: 3,
+      },
+    ],
+  };
+  
+  export default skills;
+  
