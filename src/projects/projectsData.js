bf19f554da46730472e385e0673eb9846a4679be
areeb6jamal <90337934+areeb6jamal@users.noreply.github.com>
2024-01-10 02:19:18 -0500

    Added Project Details and Minor Fixes

--- src/projects/projectsData.js
+++ src/projects/projectsData.js
@@ -1,6 +1,7 @@
 import projectOne from "../assets/project-1.png";
 import projectTwo from "../assets/project-2.png";
 import projectThree from "../assets/project-3.png";
+import projectFour from "../assets/project-4.png";
 
 const projects = {
   1: {
@@ -9,16 +10,19 @@ const projects = {
     description: (
       <>
         <p>
+        <b>Sept. 2023 - Dec. 2023</b>
+        </p>
+        <p>
         - Processed compressed data for a 320 x 240-pixel image received on an Altera DE2-115 board via UART interface and stored
         in an external SRAM.
         </p>
         <p>
-        - Developed a Verilog program using Quartus II to configure the FPGA to read and recover the compressed image to a VGA
-        controller for display to a monitor
+        - Wrote a Verilog program using Quartus II to configure the FPGA to read and recover the compressed image to a VGA
+        controller for display to a monitor.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/areeb6jamal",
   },
   2: {
     title: "Embedded 3D Spatial Mapping System",
@@ -26,12 +30,23 @@ const projects = {
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <b>Jan. 2023 - Apr. 2023</b>
+        </p>
+        <p>
+        - Integrated a 28BYJ-48 stepper motor with a VL53L1X time-of-flight sensor using infrared laser light to obtain 
+        360-degree distance measurements.
+        </p>
+        <p>
+        - Implemented a C++ program using Kiel to configure digital I/O pins on an MSP 430 microcontroller for reading
+         LiDAR sensor data through I2C serial communication.
+        </p>
+        <p>
+        - Developed a Python script to visualize time-of-flight sensor measurement data collected through UART 
+        communication from the MSP 430 in a 3D graphical representation using the comprehensive matplotlib library.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/areeb6jamal/Embedded-3D-Spatial-Mapping-System",
   },
   3: {
     title: "Artificial Pacemaker",
@@ -39,26 +54,36 @@ const projects = {
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <b>Sept. 2022 - Dec. 2022</b>
+        </p>
+        <p>
+        - Created a pacemaker model using Simulink to implement 8 different modes of autonomous heartbeat functionality for regulating 4
+        heart chambers on an NXP FRDM-K64F microcontroller.
+        </p>
+        <p>
+        - Designed a GUI using JavaScript that acted as a DCM for a user to control the pacemaker Simulink parameters as well as monitor the heart 
+        chambers through an e-gram display which was serially communicated through UART by the board.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/areeb6jamal/Artificial_Pacemaker",
   },
   4: {
     title: "Infrastructure for Self-Driving Vehicles",
     /*CHANGE LATER*/
-    image: projectOne,
+    image: projectFour,
     description: (
       <>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        <b>Jan. 2022 - Apr. 2022</b>
+        </p>
+        <p>
+        - Guided a multidisciplinary team to design and simulate a highway infrastructure using Python which allows self-driven vehicles and 
+        human-driven vehicles to efficiently integrate at different percentages of autonomous vehicles.
         </p>
       </>
     ),
-    github: "https://github.com",
+    github: "https://github.com/areeb6jamal/Infrastructure-For-Self-Driving-Vehicles",
   },
 };
 
